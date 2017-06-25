a4ee99b8f57c566f36c10e19f3409b86893fe0d6
mathworker294 <mathworker294@gmail.com>
2017-06-25 21:54:58 +0300

    Update miner.bat

--- miner.bat
+++ miner.bat
@@ -1,4 +1,13 @@
-EthDcrMiner64.exe -mode 1 -epool eu1.ethermine.org:4444 -ewal %ETH_WALLET% -eworker %computername% -epsw x -dbg -1 -r 1
+@Echo Off
+setx GPU_FORCE_64BIT_PTR 0
+setx GPU_MAX_HEAP_SIZE 100
+setx GPU_USE_SYNC_OBJECTS 1
+setx GPU_MAX_ALLOC_PERCENT 100
+setx GPU_SINGLE_ALLOC_PERCENT 100
+@Echo On
+
+EthDcrMiner64.exe -mode 1 -epool eu1.ethermine.org:4444 -ewal 0x63eD9066802675985Eedb41609374F2329773c89 -eworker %computername% -epsw x -dbg -1 -r 1
+
 # if unexpected error
 timeout 20
 reboot.bat
